80151f6fa21597ca4add1acde5e7e338840379cc
DanielMckenzie Mckenzie <drmckenzie89@gmail.com>
2018-02-10 22:59:10 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,8 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-
+end
+do
+  
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
   expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
