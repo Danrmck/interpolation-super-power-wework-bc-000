51efa52c3f2a33466de29a54f1612368889e229e
DanielMckenzie Mckenzie <drmckenzie89@gmail.com>
2018-02-10 22:54:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,6 +3,13 @@ def display_rainbow(colors)
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
   "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  expect(colors).to receive(:[]).with(0).at_least(:once).and_return("red")
+  expect(colors).to receive(:[]).with(1).at_least(:once).and_return("orange")
+  expect(colors).to receive(:[]).with(2).at_least(:once).and_return("yellow")
+  expect(colors).to receive(:[]).with(3).at_least(:once).and_return("green")
+  expect(colors).to receive(:[]).with(4).at_least(:once).and_return("blue")
+  expect(colors).to receive(:[]).with(5).at_least(:once).and_return("indigo")
+  expect(colors).to receive(:[]).with(6).at_least(:once).and_return("violet")
 end
 
 puts display_rainbow
